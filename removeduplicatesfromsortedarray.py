aa9c68f8bb5f3a7bda20f60d13521cdef360d383
MohammadShahbaaz <shahbaazmohd784@gmail.com>
2026-05-08 23:02:27 +0530

    Macbook 08/05/2026

--- removeduplicatesfromsortedarray.py
+++ removeduplicatesfromsortedarray.py
@@ -3,14 +3,14 @@ class Solution(object):
         a = 1
         for i in range(1,len(nums)):
             if nums[i] != nums[i-1]:
-                nums[a] == nums[i]
+                nums[a] = nums[i]
                 a+=1
         return a
 
 
 
 obj = Solution()
-print(obj.removeDuplicates([1,1,1]))
+print(obj.removeDuplicates([0,0,0,1,1,1,2,2,2,3,3,4]))
 
 
 
