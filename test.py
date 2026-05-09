ff14a4f3e99e0412c79dbeb59f7baf900a52f061
MohammadShahbaaz <shahbaazmohd784@gmail.com>
2026-05-09 12:23:23 +0530

    pc 09/05/2026

--- test.py
+++ test.py
@@ -1,3 +1,30 @@
-a=[1,1,2,3,4]
-a[1] = a[2]
-print(a)
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def insert(root, val):
+
+    if root is None:
+        return TreeNode(val)
+
+    if val < root.val:
+        root.left = insert(root.left, val)
+
+    else:
+        root.right = insert(root.right, val)
+
+    return root
+
+
+nums = [5, 3, 8, 1, 4]
+
+root = None
+
+for i in nums:
+    root = insert(root, i)
+
+
+print(root)
